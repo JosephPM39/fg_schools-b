1b1d631f13429e543be4844b61e1c75325a1141c
JosephPM39 <josias.jjpm49@hotmail.com>
2022-12-15 16:26:48 -0600

    Init faker class

--- src/test/fakers/model.faker.ts
+++ src/test/fakers/model.faker.ts
@@ -1,5 +1,13 @@
 import { IBaseModel } from '../../models_school/base.model'
-import { gOneFakeParams } from './types'
+import { gOneFakeParams, WithRequired } from './types'
+
+interface OneFakeParams {
+  withId?: boolean
+}
+
+interface ManyFakesParams {
+  quantity?: number
+}
 
 export const manyFakes = <Model extends IBaseModel>({
   withId,
@@ -15,4 +23,33 @@ export const manyFakes = <Model extends IBaseModel>({
     fakes.push(generateOneFake({ withId }))
   }
   return fakes
+}
+
+interface Fakes<Entity extends IBaseModel> {
+  manyWithId: Array<WithRequired<Entity, 'id'>>
+  manyWithoutId: Array<Partial<Entity>>
+  oneWithId: WithRequired<Entity, 'id'>
+  oneWithoutId: Partial<Entity>
+}
+// is necessary add condicional type for make oneFake
+export class BaseFaker<Entity extends IBaseModel, Dependencies> {
+  fakes: Fakes<Entity> = {
+    manyWithId: [],
+    manyWithoutId: []
+  }
+
+  makeOneFake: (params: OneFakeParams | Dependencies & OneFakeParams) => Partial<Entity>
+  makeManyFake = (params: ManyFakesParams & Dependencies & OneFakeParams): void => {
+    const { quantity = 100, ...remaining } = params
+    const fakes: Array<Partial<Entity>> = []
+    for (let i = 0; i < quantity; i++) {
+      fakes.push(this.makeOneFake(remaining))
+    }
+    if (params.withId) {
+      this.fakes.manyWithId = fakes
+    }
+    if (!params.withId) {
+      this.fakes.manyWithoutId = fakes
+    }
+  }
 }
